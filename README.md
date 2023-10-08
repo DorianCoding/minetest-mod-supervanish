79c27e5832f03d559894758c061ea195c017970c
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2023-10-08 14:04:29 +0200

    Mod update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# minetest-mod-vanish
-Minetest mod that allows you be invisible to other players  
-## Usage:
-* `/vanish [playername]` - make player or yourself invisible
+# minetest-mod-supervanish
+Minetest mod that allows you be invisible to other players. It can be used in singleplayer mode, *even if it is not useful*.
+
+## Usage
+* `/vanish [playername]` - make player or yourself invisible and immortal (from other players including vanish privilege users)
 * `/vanished` - show list of vanished players
-* All these commands require `vanish` priv to execute.
+* Above commands require `vanish` priv to execute.
+* `/supervanish [playername]` - make player or yourself superinvisible and immortal (noone can see you or list your name). *However you can still appear in logged users*.
+* This command require `supervanish` priv to execute.
+
+> [!NOTE]
+> It is *impossible* to change the visibility according to the player and therefore vanished people cannot be seen even by people with vanish or supervanish privilege. However, they can be listed and unvanished.
+
+## Legal
+The code is a fork from [Minetest mod vanish](https://github.com/zmv7/minetest-mod-vanish) under GPL3 and is therefore under the same license.
--- init.lua
+++ init.lua
@@ -1,76 +1,272 @@
-local s = core.get_mod_storage()
+--[[
+Author - DorianCoding 2023 (https://github.com/DorianCoding)
+
+The following code is a derivative work of the code from https://github.com/zmv7/minetest-mod-vanish by zmv7
+which is licensed GPLv3. This code therefore is also licensed under the terms 
+of the GNU Public License, version 3.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+]] local s = core.get_mod_storage()
 vanish = {}
 vanish.vanished = {}
+vanish.supervanished = {}
+vanish.protect = function(playername)
+    minetest.register_on_player_hpchange(function(player, hp_change, reason)
+        local tabl = s:to_table().fields
+        for nick, val in pairs(tabl) do
+            if val ~= "" and player:get_player_name() == nick and player:get_player_name() == playername:get_player_name() and hp_change < 0 then
+                minetest.chat_send_player(player:get_player_name(), "You are protected in vanished mode.")
+                return 0, true -- Avoid other functions to change it
+            end
+        end
+        return hp_change -- Do not alter anything
+    end, true)
+end
 vanish.on = function(player)
-	vanish.vanished[player:get_player_name()] = true
-	player:set_properties({
-		visual = "",
-		--collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
-		show_on_minimap = false,
-		pointable = false,
-	})
-	player:set_nametag_attributes({color={a=0},text = " "})
+    local name = player:get_player_name();
+    vanish.vanished[name] = true
+    player:set_properties({
+        visual = "sprinte",
+        visual_size = {
+            x = 0,
+            y = 0,
+            z = 0
+        },
+        physical = false,
+        collide_with_objects = false,
+        -- collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
+        show_on_minimap = false,
+        pointable = false,
+        is_visible = false,
+        shaded = false
+    })
+    player:set_nametag_attributes({ color = { a = 0 }, text = " " })
+end
+vanish.superon = function(player)
+    vanish.supervanished[player:get_player_name()] = true
+    player:set_properties({
+        visual = "sprinte",
+        visual_size = {
+            x = 0,
+            y = 0,
+            z = 0
+        },
+        physical = false,
+        collide_with_objects = false,
+        -- collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
+        show_on_minimap = false,
+        pointable = false,
+        is_visible = false,
+        shaded = false
+    })
+    player:set_nametag_attributes({
+        color = {
+            a = 0
+        },
+        text = " "
+    })
 end
 vanish.off = function(player)
-	vanish.vanished[player:get_player_name()] = nil
-	local name = player:get_player_name()
-	if not name then return end
-	player:set_properties({
-			 	visual = "mesh",
-	 			--collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
-			 	show_on_minimap = true,
-		 		pointable = true,
-  		})
-	if core.get_modpath("nick_prefix") then
-		nick_prefix.update_ntag(name)
-	else
-		player:set_nametag_attributes({color={a=255,r=255,g=255,b=255},text = name})
-	end
+    local name = player:get_player_name()
+    vanish.vanished[name] = nil
+    player:set_properties({
+        visual = "mesh",
+        visual_size = {
+            x = 1,
+            y = 1,
+            z = 1
+        },
+        physical = true,
+        collide_with_objects = true,
+        -- collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
+        show_on_minimap = true,
+        pointable = true,
+        is_visible = true,
+        shaded = true
+    })
+    if core.get_modpath("nick_prefix") then
+        nick_prefix.update_ntag(name)
+    else
+        player:set_nametag_attributes({
+            color = {
+                a = 255,
+                r = 255,
+                g = 255,
+                b = 255
+            },
+            text = name
+        })
+    end
 end
-
-core.register_privilege("vanish","Allows to make players invisible")
-core.register_on_joinplayer(function(player)
-	local name = player:get_player_name()
-	if not name then return end
-	local isinvis = s:get_string(name)
-	if isinvis == "1" then
-		core.after(0.1,function()
-			vanish.on(player)
-		end)
+vanish.remove = function(player, force)
+	if not s:get_string(name) then
+		return false
 	end
+	if force == True or s:get_string(name) ~= "2" then --Not in supervanish or force
+		s:set_string(player, "")
+		vanish.vanished[player] = nil
+		vanish.supervanished[player] = nil --Does not mind as if no privilege, it was not in the list
+		return true
+	end
+	return false
+end
+vanish.superoff = function(player)
+	local name = player:get_player_name()
+    vanish.supervanished[name] = nil
+    player:set_properties({
+        visual = "mesh",
+        visual_size = {
+            x = 1,
+            y = 1,
+            z = 1
+        },
+        physical = true,
+        collide_with_objects = true,
+        -- collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
+        show_on_minimap = true,
+        pointable = true,
+        is_visible = true,
+        shaded = true
+    })
+    if core.get_modpath("nick_prefix") then
+        nick_prefix.update_ntag(name)
+    else
+        player:set_nametag_attributes({
+            color = {
+                a = 255,
+                r = 255,
+                g = 255,
+                b = 255
+            },
+            text = name
+        })
+    end
+end
+core.register_privilege("vanish", {
+    description = "Allows to make players invisible",
+    give_to_singleplayer = true
+})
+core.register_privilege("supervanish", {
+    description = "Allows to make players superinvisible",
+    give_to_singleplayer = true
+})
+core.register_on_joinplayer(function(player)
+    local name = player:get_player_name()
+    if not name then
+        return
+    end
+    local isinvis = s:get_string(name)
+    if isinvis == "1" then
+        core.after(0.1, function()
+            vanish.on(player)
+        end)
+    elseif isinvis == "2" then
+        core.after(0.1, function()
+            vanish.superon(player)
+        end)
+    end
+    vanish.protect(player)
 end)
-core.register_chatcommand("vanish",{
-	description = "Toggle invisibility of player",
-	privs = {vanish=true},
-	params = "<name>",
-	func = function(name,param)
-		if param == "" then param = name end
-		local player = core.get_player_by_name(param)
-		local isinvis = s:get_string(param)
-		if isinvis == "1" then
-			s:set_string(param,"")
-			if player then
-				vanish.off(player)
+core.register_chatcommand("vanish", {
+    description = "Toggle invisibility of player",
+    privs = {
+        vanish = true
+    },
+    params = "<name>",
+    func = function(name, param)
+        if param == "" then
+            param = name
+        end
+        local player = core.get_player_by_name(param)
+		if not player then
+			local result = vanish.remove(player,false)
+			if result then
+				return false, "-!- " .. param .. " is not online but vanish dropped"
+			else
+				return false, "-!- " .. param .. " is not vanished"
 			end
-			return true, "-!- "..param.." unvanished"
-		else
-			s:set_string(param,"1")
-			if player then
-				vanish.on(player)
-			end
-			return true, "-!- "..param.." vanished"
 		end
-end})
+        local isinvis = s:get_string(param)
+        if isinvis == "1" then
+            s:set_string(param, "")
+            vanish.off(player)
+            return true, "-!- " .. param .. " unvanished"
+        elseif isinvis == "2" then
+            return false, "-!- " .. param .. " is not online but vanish dropped"
+        else
+            s:set_string(param, "1")
+            vanish.on(player)
+            return true, "-!- " .. param .. " vanished"
+        end
+    end
+})
+core.register_chatcommand("supervanish", {
+    description = "Toggle superinvisibility of player",
+    privs = {
+        supervanish = true
+    },
+    params = "<name>",
+    func = function(name, param)
+        if param == "" then
+            param = name
+        end
+        local player = core.get_player_by_name(param)
+		if not player then
+			local result = vanish.remove(player,true)
+			if result then
+				return false, "-!- " .. param .. " is not online but vanish and supervanish dropped"
+			else
+				return false, "-!- " .. param .. " is not vanished"
+			end
+		end
+        local isinvis = s:get_string(param)
+        if isinvis == "2" then
+            s:set_string(param, "")
+            vanish.superoff(player)
+            return true, "-!- " .. param .. " super-unvanished"
+        elseif isinvis == "1" then
+            return false, "-!- " .. param .. " is already on vanish mode"
+        else
+            if not player then
+                return false, "-!- " .. param .. " is not online"
+            end
+            s:set_string(param, "2")
+            vanish.superon(player)
+            return true, "-!- " .. param .. " super-vanished"
+        end
+    end
+})
 
-core.register_chatcommand("vanished",{
-	description = "Show list of vanished players",
-	privs = {vanish=true},
-	func = function(name,param)
-		local out = {}
-		local tabl = s:to_table().fields
-		for nick,val in pairs(tabl) do
-			table.insert(out,nick)
-		end
-	table.sort(out)
-		return true, "Vanished: "..table.concat(out,", ")
-end})
+core.register_chatcommand("vanished", {
+    description = "Show list of vanished players or check if a player vanished",
+    privs = {
+        vanish = true
+    },
+    params = "<name>",
+    func = function(name, param)
+        local out = {}
+        local tabl = s:to_table().fields
+        if param ~= "" then
+            if tabl[param] ~= nil then
+                if tabl[param] == "1" then
+                    return true, "The player " .. param .. " is vanished"
+                end
+            end
+            return true, "The player " .. param .. " is not vanished or offline"
+        end
+        local count = 0
+        for nick, val in pairs(tabl) do
+            if val ~= "2" then
+                count = count + 1
+                table.insert(out, nick)
+            end -- Do not take supervanish on the list
+        end
+        if count == 0 then
+            return true, "Noone is vanished"
+        end
+        table.sort(out)
+        return true, "Vanished players: " .. table.concat(out, ", ")
+    end
+})
